546f8da8294feb5ba016b3f8474dee8ebb3cc0af
esterandr02 <esterandr02@gmail.com>
2020-09-16 20:51:46 -0300

    declared getAllAppointments function

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -12,4 +12,5 @@ export default interface IAppointmentsRepository {
     findAllHoursInDay(
         data: IListProviderHoursAvailableDTO,
     ): Promise<Appointment[]>;
+    getAllAppointments(): Promise<Appointment[]>;
 }
