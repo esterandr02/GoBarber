71236a22798f66aff138801b30b3af8acbc9635f
esterandr02 <esterandr02@gmail.com>
2020-09-16 20:49:28 -0300

    added list appointments route

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,14 +1,16 @@
-import CreateAppointmentsService from '@modules/appointments/services/CreateAppointmentsService';
-
 import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+
+import CreateAppointmentsService from '@modules/appointments/services/CreateAppointmentsService';
+import ListAppointmentsService from '@modules/appointments/services/ListAppointmentsService';
 
 export default class AppointmentsController {
     public async create(
         request: Request,
         response: Response,
     ): Promise<Response> {
+        const { user_id } = request.params;
         const { provider_id, date } = request.body;
         const parsedDate = parseISO(date);
 
@@ -16,8 +18,20 @@ export default class AppointmentsController {
 
         const appointment = await createAppointments.execute({
             provider_id,
+            user_id,
             date: parsedDate,
         });
         return response.json(appointment);
+    }
+
+    public async index(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const listAppointments = container.resolve(ListAppointmentsService);
+
+        const appointments = await listAppointments.execute();
+
+        return response.json(appointments);
     }
 }
