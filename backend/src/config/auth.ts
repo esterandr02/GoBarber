af85182ffa439ba8103a554773f6c4fc9146c54f
esterandr02 <esterandr02@gmail.com>
2020-09-27 13:41:07 -0300

    jwt enviroment variable used

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,13 @@
+interface IJwt {
+    jwt: {
+        secret: string;
+        expiresIn: string;
+    };
+}
+
 export default {
     jwt: {
-        secret: process.env.APP_SECRET,
-        expiresIn: '1d',
+        secret: process.env.APP_SECRET || 'default',
+        expiresIn: process.env.APP_SECRET_EXPIRES_IN || '1d',
     },
-};
+} as IJwt;
