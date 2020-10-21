3bb4e908b47e6c977f58bf09221af4244bc3239c
esterandr02 <esterandr02@gmail.com>
2020-10-21 19:01:12 -0300

    new S3 driver option added

--- backend/src/shared/container/providers/Storage/index.ts
+++ backend/src/shared/container/providers/Storage/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
 import IStorageFileProvider from './model/IStorageFileProvider';
+
+import S3StorageProvider from './implementations/S3StorageProvider';
 import DiskStorageFileProvider from './implementations/DiskStorageFileProvider';
+
+const providers = {
+    disk: DiskStorageFileProvider,
+    s3: S3StorageProvider,
+};
 
 container.registerSingleton<IStorageFileProvider>(
     'StorageFileProvider',
-    DiskStorageFileProvider,
+    providers.s3,
 );
