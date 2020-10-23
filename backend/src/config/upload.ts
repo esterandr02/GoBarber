8a8ee99f782d26634f32b33b961a189390021b91
esterandr02 <esterandr02@gmail.com>
2020-10-23 01:42:50 -0300

    multer config uncoupled and separated

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,21 +1,50 @@
 import path from 'path'; // tratar a variavel de ambiente nos diversos ambiemtes (so).
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp'); // pasta destino dos arquivos de img
 
+interface IUploadConfig {
+    driver: 's3' | 'disk';
+
+    tmpFolder: string;
+    uploadsFolder: string;
+
+    multer: {
+        storage: StorageEngine;
+    };
+
+    config: {
+        disk: {};
+        s3: {
+            bucket: string;
+        };
+    };
+}
+
 export default {
+    driver: process.env.STORAGE_DRIVER,
+
     tmpFolder,
     uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
+    multer: {
+        storage: multer.diskStorage({
+            destination: tmpFolder,
 
-        filename(request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString('HEX'); // string de criptografia em hexadecimal
-            const fileName = `${fileHash}-${file.originalname}`; // nome final do arquivo de img
+            filename(request, file, callback) {
+                const fileHash = crypto.randomBytes(10).toString('HEX'); // string de criptografia em hexadecimal
+                const fileName = `${fileHash}-${file.originalname}`; // nome final do arquivo de img
 
-            return callback(null, fileName);
+                return callback(null, fileName);
+            },
+        }),
+    },
+
+    config: {
+        disk: {},
+        s3: {
+            bucket: 'ester-gobarber',
         },
-    }),
-};
+    },
+} as IUploadConfig;
